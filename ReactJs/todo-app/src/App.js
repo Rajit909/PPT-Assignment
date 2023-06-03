802a5671fc64211cdaac85fef1db54e17ce67a05
Rajit Kumar <krajit909@gmail.com>
2023-06-03 13:33:25 +0530

    bring componet in app.js

---
+++ ReactJs/todo-app/src/App.js
@@ -0,0 +1,41 @@
+import React, { useReducer } from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+const initialState = {
+  todos: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        todos: [
+          ...state.todos,
+          { id: Date.now(), text: action.payload },
+        ],
+      };
+    case 'DELETE_TODO':
+      return {
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    default:
+      throw new Error('Unsupported action type');
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoForm dispatch={dispatch} />
+      {state.todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
