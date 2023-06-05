b011cdd47d73f38e030e2c380f39ab09543f98be
Rajit Kumar <94344369+Rajit909@users.noreply.github.com>
2023-06-05 08:35:00 +0000

    Login.js completed

---
+++ ReactJs/task-manager/src/Login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const Login = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Make a POST request to the authentication endpoint
+    const response = await fetch('https://reqres.in/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      const { token } = await response.json();
+      onLogin(token); // Pass the token to the parent component
+    } else {
+      // Handle invalid login
+      console.log('Invalid login');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
